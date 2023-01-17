a3f44647e0814b16112fe0859ea618116bda6102
unknown <wyller_gt@hotmail.com>
2023-01-17 08:57:41 -0300

    fix: ajustando alguns arquivos de configurações 2

--- Backend_Sem_Login/src/app.ts
+++ Backend_Sem_Login/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+
 import productsRoutes from "./routers/products.routes";
 import ibgeRoutes from "./routers/ibge.routes";
 const app = express();
